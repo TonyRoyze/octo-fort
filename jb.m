0cd4387482ac15e154eace901e1460cc07d4fb8e
Vidura Gunawardana <101244400+TonyRoyze@users.noreply.github.com>
2025-02-19 14:48:40 +0530

    Jacobi Iteration

--- jb.m
+++ jb.m
@@ -1,11 +1,17 @@
 function X = jacobi(A,B,P,max1)
     N = length(B);
-    %X = zeros(1,N)
+    X = zeros(1,N);
     for k = 1: max1
         for j =1:N
-            X(j) = (B(j) - A(j,[1:j-1,j+1:N])*P([1:j-1,j+1:N]))/A(j,j)
+            X(j) = (B(j) - A(j,[1:j-1,j+1:N])*P([1:j-1,j+1:N]))/A(j,j);
         endfor
+        % err = norm(X - P);
+        % if (err < 0.0001)
+        %     break;
+        % endif
         P = X';
     endfor
     X = X';
 endfunction
+
+jacobi(A,b,[1;2;2],30)
