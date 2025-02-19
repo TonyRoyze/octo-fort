7c28efd393528c3dd51297ac67754503a1161e5a
Vidura Gunawardana <101244400+TonyRoyze@users.noreply.github.com>
2025-02-19 19:29:46 +0530

    Generalized Inverse

--- gi.m
+++ gi.m
@@ -1,6 +1,6 @@
 %gi
 rankA = rank(A);
-detA = det(A);
+%detA = det(A);
 
 C = [A(1:2, 1:2)];
 %rankC = rank(C);
@@ -8,12 +8,12 @@ D = inv(C);
 G = [D(1,1) D(1,2) 0; D(2,1) D(2,2) 0; 0 0 0]
 
 
-if A*G*A = A
+if A*G*A == A
     disp('G is the generalized inverse of A')
 else
     disp('G is not the generalized inverse of A')
 end
 
-x = Gb;
+x = G*b;
 disp('The solution of the equation Ax = b is:');
 x
