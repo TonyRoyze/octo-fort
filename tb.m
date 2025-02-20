49b421e64300ebdabb8ac8562b6b01b8dbcd2423
Vidura Gunawardana <101244400+TonyRoyze@users.noreply.github.com>
2025-02-20 19:59:24 +0530

    Table Layout

---
+++ tb.m
@@ -0,0 +1,4 @@
+fprintf('%-10s %-10s %-10s %-10s\n', 'Symbol', 'x', 'y', 'alpha');
+fprintf('-----------------------------------------\n');
+fprintf('%-10s %-10.5f %-10.5f %-10.2f\n', 'None', x(1), y(1), rad2deg(a(1)));
+fprintf('%-10s %-10.5f %-10.5f %-10.2f\n', w(i), x(index), y(index), rad2deg(a(index)));
