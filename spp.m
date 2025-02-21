2785ff13e0283592db7d90ec77ed814e73cb3bce
Vidura Gunawardana <101244400+TonyRoyze@users.noreply.github.com>
2025-02-21 08:17:18 +0530

    Scaled Partial Pivoting

--- spp.m
+++ spp.m
@@ -1,8 +1,7 @@
-%spp
 Aug = [A, b]
 
 [nRows, nCols] = size(Aug);
-S = max(abs(Aug(:,1:end-1)), [], 2);
+S = max(abs(Aug(:,1:end-1)), [], 2)
 
 
 for j = 1:nCols-2
@@ -11,7 +10,8 @@ for j = 1:nCols-2
     fprintf('Maximum of column %i is %i.\n', j , maximum);
     if j ~= rel_index+j-1
         fprintf('Swapping row %i with %i.\n', j, rel_index+j-1);
-        Aug([j, rel_index+j-1], :) = Aug([rel_index+j-1, j], :);
+        Aug([j, rel_index+j-1], :) = Aug([rel_index+j-1, j], :)
+        S([j, rel_index+j-1], :) = S([rel_index+j-1, j], :)
     end
 
     for i = j+1:nRows
